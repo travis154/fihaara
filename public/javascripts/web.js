a6ba89c9287e5453dc07ffd2b1e835c0eed451bf
demon <maldivianist@gmail.com>
2014-01-25 02:10:06 +0500

    fixed height partially

--- public/javascripts/web.js
+++ public/javascripts/web.js
@@ -1,6 +1,8 @@
 
 
 $(function(){
+
+	//Treemap algorithm contributed by Azwad
 	var btn = '<button type="button" style="cursor:col-resize" class="resize-horizontal">divide horizontal</button><button type="button" style="cursor:row-resize" class="resize-vertical">divide vertical</button>';
 	$("body").on('click', '.resize-horizontal, .resize-vertical', function(e){
 		var type = $(this).hasClass("resize-horizontal") ? "horizontal" : "vertical";
@@ -44,11 +46,14 @@ $(function(){
 		element2.css("background", col);
 		element2.append(btn);
 		
+		var sibling = ~~(Math.random()*50000000);
+		element1.attr("data-sibling", sibling);
+		element2.attr("data-sibling", sibling);
+		
 		box.remove();
 		$(".treemap").append(element1);
 		$(".treemap").append(element2);		
 	});
-	
 	$("body").on('click','.height-add, .height-remove', function(){
 		var el = $(this);
 		var val = el.hasClass("height-add") ? 10 : -10;
@@ -63,123 +68,20 @@ $(function(){
 			return parseInt($(e).attr("data-level"));
 		});
 		console.log({position:position_sorted, height:height_sorted, level:level_sorted});
-		
-		
-		//sort by height
 		var c = 1;
 		for(var h in height_sorted){
-			var n = Math.pow(2,c) * 10;
+			var n = Math.pow(2,c) * val;
 			height_sorted[h].forEach(function(e){
 				var self = $(e);
 				var height = parseInt(e.style.height);
+				var top = parseInt(e.style.top);
 				var new_height = height + n;
 				self.css("height", new_height + "px");
+				if(top!=0){
+					self.css("top", new_height + "px");
+				}
 			});
 			c += 1;
-		}
-		return;
-		
-		//add height by level
-		var level_sorted = $(".treemap div").sort(function(a,b){
-			return parseInt($(a).attr("data-level")) -  parseInt($(b).attr("data-level"));
-		});
-		var height_sorted = $(".treemap div").sort(function(a,b){
-			return parseInt(a.style.height) +  parseInt(b.style.height);
-		});
-		var clvl;
-		var vl=1;
-		//var sorted = Array.prototype.reverse.apply(level_sorted);
-		var sorted = level_sorted;
-		console.log(height_sorted);
-		
-		var last_height;
-		var level;
-		
-		height_sorted.each(function(){
-			var self = $(this);
-			var height = parseInt(this.style.height);
-			
-			if(!last_height){
-				last_height = height;
-				level = 1;
-			}
-			
-			if(height > last_height){
-				level += 1;
-			}
-			
-			var new_height = height + (Math.pow(2,level)*10);
-			self.css("height", new_height + "px");
-			
-		});
-		return;
-		sorted.each(function(){
-			var self = $(this);
-			var height = parseInt(this.style.height);
-			var level = parseInt(self.attr("data-level"));
-			if(!clvl){
-				clvl = level;
-			}else if(clvl != level){
-				clvl = level;
-				vl += 10;
-			}
-			var new_height = height + (Math.pow(2,clvl)*10);
-			self.css("height", new_height + "px");
-			var data = {
-				height:height,
-				new_height:new_height,
-				height_change:new_height-height,
-				//top:top,
-				level:level,
-				//new_top:new_top
-			};
-			self.find("small").remove();
-			self.append('<small style="font-size:10px">'+JSON.stringify(data,true,3)+'</small>');
-			return;
-			var top = parseInt(this.style.top);
-			var new_top = top + (top == 0 ? top : val+val);
-			self.css("top", new_top + "px");
-		});
-		
-		
-		return;
-		var add = val;
-		delete position_sorted['0'];
-		for(var i in position_sorted){
-			var arr = position_sorted[i];
-			if(arr.length){
-				arr.forEach(function(el){
-					var self = $(el);
-					var level = parseInt(self.attr("data-level"));
-					var top = parseInt(el.style.top);
-					//has a sibling?
-					//is it same height?
-					//add sum of its height to mine					
-					if(top != 0){
-						self.css("top", (top+add) + "px");
-					}				
-				});
-				add = add + val;
-				
-				/*
-				arr.forEach(function(el){
-					var self = $(el);
-					var height = parseInt(el.style.height);
-					var top = parseInt(el.style.top);
-					var new_height = height + val;
-					self.css("height", new_height + "px");
-					var level = parseInt(self.attr("data-level"));
-					//has a sibling?
-					//is it same height?
-					//add sum of its height to mine		
-					var nlen = arr.length > 1 ? add * level : add;	
-					console.log(nlen);		
-					if(top != 0){
-						self.css("top", (top+nlen) + "px");
-					}				
-				});
-				*/
-			}
 		}
 	});
 	
